afa4cfae4fe12bd59370e45b83de8c86f55ef859
oleksmir <olexiy.miroshnik@gmail.com>
2017-12-01 13:34:56 +0200

    Add basic interface component using material

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,26 +1,52 @@
 import {NgModule} from '@angular/core';
-import {AppComponent} from './app.component';
 import {CommonModule} from '@angular/common';
+import {
+  MatButtonModule, MatCardModule, MatIconModule, MatListModule, MatSidenavModule,
+  MatToolbarModule
+} from '@angular/material';
+import {RouterModule} from '@angular/router';
 
 import * as firebase from 'firebase';
 import {config} from '../../config/firebase.config';
 import {FirestoreConnector} from 'a2c-firestore-connector';
-import {ProductsModule} from 'a2c-products';
+
+import {AppComponent} from './containers/app.component';
 import {ChildProductsModule} from '../child-products/child-products.module';
+import {NotFoundPageComponent} from './containers/not-found-page.component';
+import {LayoutComponent} from './components/layout';
+import {NavItemComponent} from './components/nav-item';
+import {SidenavComponent} from './components/sidenav';
+import {ToolbarComponent} from './components/toolbar';
 
 firebase.initializeApp(config);
 
+const COMPONENTS = [
+  AppComponent,
+  NotFoundPageComponent,
+  LayoutComponent,
+  NavItemComponent,
+  SidenavComponent,
+  ToolbarComponent,
+];
+
+const MATERIAL = [
+  MatButtonModule,
+  MatCardModule,
+  MatIconModule,
+  MatSidenavModule,
+  MatListModule,
+  MatToolbarModule,
+];
+
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
+  declarations: COMPONENTS,
   imports: [
     CommonModule,
+    ...MATERIAL,
+    RouterModule,
     ChildProductsModule,
   ],
-  exports: [
-    AppComponent,
-  ],
+  exports: COMPONENTS,
   providers: [
     { provide: 'IConnector', useClass: FirestoreConnector}
   ],
