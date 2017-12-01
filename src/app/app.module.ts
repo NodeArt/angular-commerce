c1f618530b129ede4b1cdc5410a5a1d2233ee20c
oleksmir <olexiy.miroshnik@gmail.com>
2017-12-01 12:44:23 +0200

    Add basic modules to app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,32 @@ import {NgModule} from '@angular/core';
 import {EffectsModule} from '@ngrx/effects';
 import {StoreModule} from '@ngrx/store';
 
-import {AppComponent} from './core/app.component';
+import {AppComponent} from './core/containers/app.component';
 import {CoreModule} from './core/core.module';
 import {RouterModule} from '@angular/router';
 import {routes} from './routes';
+import {CommonModule} from '@angular/common';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import {AuthModule} from 'a2c-auth';
+import {FirebaseAuth} from 'a2c-auth-firebase';
+import {FirestoreConnector} from 'a2c-firestore-connector';
 
 @NgModule({
   declarations: [],
   imports: [
+    CommonModule,
     BrowserModule,
-    CoreModule,
+    BrowserAnimationsModule,
     RouterModule.forRoot(routes),
     StoreModule.forRoot({}),
     EffectsModule.forRoot([]),
+    CoreModule,
+    AuthModule.forRoot(),
+  ],
+  providers: [
+    { provide: 'IConnector', useClass: FirestoreConnector },
+    { provide: 'IAuth', useClass: FirebaseAuth },
   ],
   bootstrap: [AppComponent]
 })
