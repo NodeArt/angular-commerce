05b34d5c4c109876ffc4e86cd38824bd9528ea97
oleksmir <olexiy.miroshnik@gmail.com>
2017-12-01 13:37:30 +0200

    Add not found page

---
+++ src/app/core/containers/not-found-page.component.ts
@@ -0,0 +1,21 @@
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+
+@Component({
+  selector: 'app-not-found-page',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  template: `
+    <mat-card>
+      <mat-card-title>404: Not Found</mat-card-title>
+      <mat-card-content>
+        <p>Hey! It looks like this page doesn't exist yet.</p>
+      </mat-card-content>
+      <mat-card-actions>
+        <button mat-raised-button routerLink="/">Take Me Home</button>
+      </mat-card-actions>
+    </mat-card>
+  `,
+  styles: [``]
+})
+export class NotFoundPageComponent {
+
+}
